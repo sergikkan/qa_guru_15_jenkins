d8d9ac39378ecba4d3a16d900726fbdd1137817b
user <sergik-199022@mail.ru>
2022-10-24 10:09:06 +0600

    added selenoid

--- src/test/java/org/skan/tests/BaseTest.java
+++ src/test/java/org/skan/tests/BaseTest.java
@@ -36,5 +36,6 @@ public class BaseTest {
         Configuration.holdBrowserOpen=true;
         //Configuration.pageLoadTimeout=150000;
         Configuration.browserSize="1920x1080";
+        Configuration.remote="https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
